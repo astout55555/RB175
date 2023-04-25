d8614cdd9715bfdf64014539eead28cf1dcd7d17
astout55555 <alex.the.stout5@gmail.com>
2023-04-25 15:49:33 -0700

    Finished going through guided book_viewer project. Had a lot of trouble with getting the search to return specific paragraphs, in part because I didn't realize that the element IDs only need to be unique on that page, not the whole website--it's okay if paragraph 10 from chapter 1 and paragraph 10 from chapter 2 have the same ID, as long as you aren't trying to display them both on the same webpage. I'll finish the code challenge and the rest of the lesson tomorrow.

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,22 +2,76 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("./data/toc.txt")  
+end
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name, :number, and
+# :paragraphs keys. The value for :paragraphs will be a hash of paragraph indexes
+# and that paragraph's text.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+helpers do
+  def highlight(text, substring)
+    text.gsub(substring, "<strong>#{substring}</strong>")
+  end
+
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |paragraph, index|
+      "<p id=paragraph#{index}>#{paragraph}</p>"
+    end.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("../data/toc.txt")
+
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("../data/toc.txt")
-  @chp_num = params[:number].to_i
-  @chp_name = @contents[@chp_num - 1]
-  @title = "Chapter #{@chp_num}: #{@chp_name}"
-  @chapter = File.read("../data/chp#{@chp_num}.txt")
+  chp_num = params[:number].to_i
+  chp_name = @contents[chp_num - 1]
+  
+  redirect "/" unless (1..@contents.size).cover? chp_num
+  
+  @title = "Chapter #{chp_num}: #{chp_name}"
+  @chapter = File.read("./data/chp#{chp_num}.txt")
 
   erb :chapter
 end
 
-get "/show/:name" do
-  "<h1>#{params[:name]}</h1>"
+get "/search" do
+  @results = chapters_matching(params[:query])
+
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
